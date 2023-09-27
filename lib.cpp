5bc2f8ccecbf2cb874c12402e23629fcd9743aca
Evie Pussifresh <pcvvblah@gmail.com>
2023-09-27 16:33:04 -0600

    Mas automatas :v
    when te pones bn papupapu

--- lib.cpp
+++ lib.cpp
@@ -3,6 +3,24 @@
 #include<string>
 using namespace std;
 #include<cctype>
+#include <set>
+
+
+
+set<string> keywords= {
+    "if", "switch",
+    "else", "si",
+    "while",
+    "for",
+    "with",
+    // TODO: Agreguen las keywords en español
+
+};
+
+set<char> types= {
+    'n','t',92, '"'};
+bool is_keyword(string s){return keywords.find(s) != keywords.end();}
+bool is_bs(char s){return types.find(s) != types.end();}
 // Es numero
 bool is_number(char _){return isdigit(_);}
 // Es mayuscula
@@ -10,21 +28,40 @@ bool is_mayus(char _){return isupper(_);}
 // Es minuscula
 bool is_minus(char _){return islower(_);}
 // Es letra valida
+
+// Es "
+bool is_q_mark(char _){return _=='"';}
+/* Es \  */
+bool is_backslash(char _){return _=='\\';}
+
 bool is_letter(char _){return is_mayus(_) || is_minus(_);}
 // Cuenta como primero
         // Metan aqui los criterios necesarios
 bool is_valid_firstChar(char _) {return is_letter(_) || _=='_';}
 
 bool is_hex(char _){return is_number(_) || _=='A' || _=='a' || _=='B' || _=='b' || _=='C' || _=='c' || _=='D' || _=='d'|| _=='E' || _=='e' || _=='F' || _=='f';}
+bool is_bin(char _){return _=='0' || _=='1';}
+class str{
+    public:
+        const char* cadena;
+        int s = 0;
+        int len;
+};
+bool is_dot(char _){return _=='.';}
+str init(string _s){
+    str _S = str();
+    _S.cadena = _s.c_str();
+    _S.len = strlen(_S.cadena);
+    return _S;
 
+}
 // Es un identificador
 bool is_valid_ID(string _s){
-    const char* _S = _s.c_str();
+    str item = init(_s);
     int s = 0;
-    int len = strlen(_S);
-    if (len == 0) return false;
-    for (int i = 0; i < len; i++){
-        char c = _S[i];
+    if (item.len == 0) return false;
+    for (int i = 0; i < item.len; i++){
+        char c = item.cadena[i];
         // cout<<"\nEstado: "<<s<<" Caracter: "<<c<<"\n";
         switch (s)
         {
@@ -43,12 +80,11 @@ bool is_valid_ID(string _s){
 }
 // Es un identificador
 bool is_int(string _s){
-    const char* _S = _s.c_str();
+    str item = init(_s);
     int s = 0;
-    int len = strlen(_S);
-    if (len == 0) return false;
-    for (int i = 0; i < len; i++){
-        char c = _S[i];
+    if (item.len == 0) return false;
+    for (int i = 0; i < item.len; i++){
+        char c = item.cadena[i];
         // cout<<"\nEstado: "<<s<<" caracter: "<<c<<'\n';
         // cout<<
         switch (s)
@@ -71,12 +107,72 @@ bool is_int(string _s){
         // Es hexadecimal
         case 3:
             if (is_hex(c)) s=3;else return false;break;
+        // Es binario
         case 4:
-            if (c=='0' || c=='1') s=4;else return false;break;
+            if (is_bin(c)) s=4;else return false;break;
         default: return false;
         }
     }
     return s==1 || s == 3 || s==4;
+}
+bool is_string(string _s){
+    str item = init(_s);
+    int s = 0;
+    if (item.len == 0) return false;
+    for (int i = 0; i < item.len; i++){
+        char c = item.cadena[i];
+        // cout<<"\nEstado actual: "<<s<<endl;
+        switch (s){
+            case 0:
+                if (is_q_mark(c)) s=1;
+                else return false;
+                break;
+            case 1:
+                if (is_backslash(c)) s=2;
+                else if (is_q_mark(c)) s=3;
+                else s=1;
+                // se vale cualquier caracter
+                break;
+            case 2:
+                if (is_bs(c)) s=1;
+                else return false;
+                break;
+            case 3: 
+                if (c==' ') s=3;
+                break;
+            default: return false;
+        }
+        // cout<<"\nCaracter: "<<c<<"\nEstado nuevo: "<<s<<endl;
+    }
+    return s==3;
+}
+bool is_float(string _s){
+    str item = init(_s);
+    int s = 0;
+    if (item.len == 0) return false;
+    // Si es entero tambien cuenta
+    if (is_int(_s)) return true;
+    for (int i = 0; i < item.len; i++){
+        char c = item.cadena[i];
+        switch (s){
+            case 0:
+                if (is_dot(c)) s=2;
+                else if (is_number(c)) s=1;
+                else return false;
+                break;
+            case 1:
+                if (is_number(c)) s=1;
+                else if (is_dot(c)) s=2;
+                else return false;
+                break;
+            case 2:
+                if (is_number(c)) s=1;
+                else return false;
+                break;
+            default: return false;
+        }
+    }
+    return s==1;   
 }
 
 
@@ -97,14 +193,26 @@ void salida(string _, bool condicion){cout<<_<<" es ";if (!condicion) cout<<"in"
 void automata_numero()
 {
     system("cls");cout<<"escribe un numero entero\n\tEjemplo: 0b01101011, 0xab32A, 43\n\n\t>>";
-    string  _s = "";cin>>_s;salida(_s, is_int(_s));
+    string  _s = "";getline(cin, _s);salida(_s, is_int(_s));
     
 }
 void automata_variable()
 {
     system("cls");cout<<"escribe una variable\n\tEjemplo: _chiludo, HolaMundo, evieChiluda43\n\n\t>>";
-    string  _s = "";cin>>_s;salida(_s, is_valid_ID(_s));
+    string  _s = "";getline(cin, _s);salida(_s, is_valid_ID(_s));
     
+}
+void automata_float(){
+    system("cls");cout<<"escribe un numero\n\tEjemplo:3.2 3\n\n\t>>";
+    string  _s = "";getline(cin, _s);salida(_s, is_float(_s));
+}
+void automata_keyword(){
+    system("cls");cout<<"escribe una palabra reservada\n\tEjemplo: if, else, with\n\n\t>>";
+    string  _s = "";getline(cin, _s);salida(_s, is_keyword(_s));
+}
+void automata_string(){
+    system("cls");cout<<"escribe una cadena\n\tEjemplo: if, else, with\n\n\t>>";
+    string  _s = "";getline(cin, _s);salida(_s, is_string(_s));
 }
 
 // const char* t[5] = {"00", "_32", "My var", "MyVar", "myVar"};
--- main.cpp
+++ main.cpp
@@ -2,24 +2,37 @@
 using namespace std;
 #include "lib.cpp"
 #include "class.cpp"
-
 int main(){
-    cout<<"Te saluda el archivo nativo.\n";
+/*  cout<<"Te saluda el archivo nativo.\n";
     if (is_mayus('a'))cout<<"Te saluda la condicional if (is_mayus('a')) ...sorra";
     else cout<<"Te saluda el else perra\n";
     _class cl = _class();
     cl.saludar();
 // 32 en hexadecimal
     int _32 = 0x20;
+  
+    string cc;
+    getline(cin,cc);
+    if (is_bs(cc[0]))cout<<"valido\n";
+    */
+    
     while(true){
         string num = "";
-        cout<<"Que desea hacer?\n1- Automata de numeros\n2-Automata de variables\n\t>>";
-        cin>>num;
+        cout<<"Que desea hacer?\n\
+        1- Automata de numeros\n\
+        2-Automata de variables\n\
+        3-Automata de numeros float\n\
+        4-Automata de palabras reservadas\n\
+        5-Automata de cadenas\n\
+        \t>>";
+        getline(cin, num);
         switch (num[0]){
             case '0': return 0;
             case '1': automata_numero();break;
             case '2': automata_variable();break;
-
+            case '3': automata_float();break;
+            case '4': automata_keyword();break;
+            case '5': automata_string();break;
         }
     }
     return 0;
